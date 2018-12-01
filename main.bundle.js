7d5c3968b98c6757be180f3349cd1d34f580fc37
Tara Craig <tara.craig7@gmail.com>
2018-12-01 16:13:03 -0700

    Compile information for deployment.

--- main.bundle.js
+++ main.bundle.js
@@ -46,8 +46,51 @@
 
 	"use strict";
 
-	// This file is in the entry point in your webpack config.
+	var getSenators = function getSenators() {
+	  var q = $(".dropdown").val();
+	  fetch("https://thecongresstracker.herokuapp.com/api/v1/members?chamber=senate&state=" + q).then(function (response) {
+	    return response.json();
+	  }).then(function (parsedResponse) {
+	    return compileSenators(parsedResponse);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	var compileSenators = function compileSenators(senators) {
+	  senators.forEach(function (senator) {
+	    displaySenators(senator);
+	  });
+	};
+
+	var displaySenators = function displaySenators(senator) {
+	  $('#senators').append("\n    <article class=\"individual-index\">\n      <p class=\"name-link sen-name\">" + senator.name + "</p>\n      <p class=\"sen-id\">" + senator.id + "</p>\n      <p class=\"sen-party\">" + senator.party + "</p>\n      <p class=\"sen-twitter\">" + senator.twitter + "</p>\n      <p class=\"sen-facebook\">" + senator.facebook + "</p>\n      <p class=\"sen-seniority\">" + senator.seniority + "</p>\n      <p class=\"sen-next-election\">" + senator.next_election + "</p>\n    </article>");
+	};
+
+	var getRepresentatives = function getRepresentatives() {
+	  var q = $(".dropdown").val();
+	  fetch("https://thecongresstracker.herokuapp.com/api/v1/members?chamber=house&state=" + q).then(function (response) {
+	    return response.json();
+	  }).then(function (parsedResponse) {
+	    return compileRepresentatives(parsedResponse);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	var compileRepresentatives = function compileRepresentatives(representatives) {
+	  representatives.forEach(function (representative) {
+	    displayRepresentatives(representative);
+	  });
+	};
+
+	var displayRepresentatives = function displayRepresentatives(representative) {
+	  $('#representatives').append("\n    <article class=\"individual-index\">\n      <p class=\"name-link rep-name\">" + representative.name + "</p>\n      <p class=\"rep-id\">" + representative.id + "</p>\n      <p class=\"rep-party\">" + representative.party + "</p>\n      <p class=\"rep-twitter\">" + representative.twitter + "</p>\n      <p class=\"rep-facebook\">" + representative.facebook + "</p>\n      <p class=\"rep-seniority\">" + representative.seniority + "</p>\n      <p class=\"rep-next-election\">" + representative.next_election + "</p>\n    </article>");
+	};
+
 	$("#find-button").on("click", function () {
+	  getRepresentatives();
+	  getSenators();
 	  $('.search-container').slideUp(900);
 	  $('.index-container').slideDown();
 	});
